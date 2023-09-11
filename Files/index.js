5efee1eefb5d6d5e2e5164742b55f2e2c35d1354
VideoCheese3773 <eedduxx@gmail.com>
2023-09-11 10:42:55 -0500

    working!

---
+++ Files/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const port = 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+io.emit('some event', {
+    someProperty: 'some value',
+    otherProperty: 'other value'
+});
+
+server.listen(port, () => {
+    console.log('server running at port:', port)
+});
